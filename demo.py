6808d0d2ecfcbd4af8288501047a844331ac54be
rookbee91 <rookbee91@gmail.com>
2023-10-07 11:55:15 +0530

    Initial commit

---
+++ demo.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title('hello streamlit')
+
+st.header('myheader')
+
+st.subheader('this is a subheader')
+st.text("abhigyan's first streamlit project")
+
+st.markdown("""
+    # h1 tag
+    ## h2 tag
+    ### h3 tag
+    """)
+
+st.write("abhigyan","datta","python")
